e3441607c1032450b7cd4f76afcad51d20471899
Borja Souto <borjasoutoprego@gmail.com>
2021-04-19 11:56:34 +0200

    Segunda iteración

--- ServiciosWeb/ServiciosWeb.py
+++ ServiciosWeb/ServiciosWeb.py
@@ -15,12 +15,29 @@ soup = BeautifulSoup(resp.content.decode(), 'html.parser') # decodificamos el co
 dict = {}
 for element in soup.find_all('tr'):
         columnas = element.find_all('th')
-        #print(columnas[1].get_text(), columnas[0].get_text())
         dict[columnas[1].get_text()] = columnas[0].get_text()
         
-print(townhall_name, dict[townhall_name])
+print('Ayuntamiento:', townhall_name, '\nCódigo:', dict[townhall_name])
+identificador = dict[townhall_name]
         
         
-        
-                
-        
+sky_resp = requests.get(f'http://servizos.meteogalicia.gal/rss/predicion/jsonPredConcellos.action?idConc={identificador}')    
+
+sky_resp = sky_resp.json()   
+
+
+sky_data = str(sky_resp['predConcello']['listaPredDiaConcello'][0]['ceo']['manha'])
+
+    
+code_sky = requests.get('https://irdgcdinfo.data.blog/codigos/')
+soup2 = BeautifulSoup(code_sky.content.decode(), 'html.parser')
+
+dict2 = {}
+for element2 in soup2.find_all('tr'):
+    columnas2 = element2.find_all('th')
+
+    dict2[columnas2[0].get_text()] = columnas2[1].get_text()
+
+  
+sky_state = dict2[sky_data]
+print('Estado del cielo por la mañana:', sky_state)
