e7fa10d89244ec6b6f673d7db5da5b9c67a0f196
Borja Souto <borjasoutoprego@gmail.com>
2021-04-21 10:41:08 +0200

    Cuarta iteración sin finalizar

--- ServiciosWeb/ServiciosWeb.py
+++ ServiciosWeb/ServiciosWeb.py
@@ -7,49 +7,63 @@ Created on Fri Apr  9 09:20:01 2021
 
 import requests
 from bs4 import BeautifulSoup
+from gmg import gmg
+import time
 
-townhall_name = 'Vigo'
-resp = requests.get('https://irdgcdinfo.data.blog/ayuntamientos/')
-soup = BeautifulSoup(resp.content.decode(), 'html.parser') # decodificamos el contenido de la respuesta para que lo lea como texto
+townhall_list = ['Verín', 'Vigo', 'Santiago de Compostela', 'Burela', 'Viveiro', 'Ponteceso', 'Lugo', 'Guitiriz', 'Tui', 'Ribadavia']
+map_list = []
 
-dict = {}
-for element in soup.find_all('tr'):
-        columnas = element.find_all('th')
-        dict[columnas[1].get_text()] = columnas[0].get_text()
-        
-print('Ayuntamiento:', townhall_name, '\nCódigo:', dict[townhall_name])
-identificador = dict[townhall_name]
-        
-        
-sky_resp = requests.get(f'http://servizos.meteogalicia.gal/rss/predicion/jsonPredConcellos.action?idConc={identificador}')    
+for townhall in townhall_list:
 
-sky_resp = sky_resp.json()   
+    townhall_name = townhall 
+    resp = requests.get('https://irdgcdinfo.data.blog/ayuntamientos/')
+    soup = BeautifulSoup(resp.content.decode(), 'html.parser') 
+    
+    dict = {}
+    for element in soup.find_all('tr'):
+            columnas = element.find_all('th')
+            dict[columnas[1].get_text()] = columnas[0].get_text()
+            
+    print('Ayuntamiento:', townhall_name, '\nCódigo:', dict[townhall_name])
+    identificador = dict[townhall_name]
+            
+            
+    sky_resp = requests.get(f'http://servizos.meteogalicia.gal/rss/predicion/jsonPredConcellos.action?idConc={identificador}')    
+    
+    sky_resp = sky_resp.json()   
+    
+    sky_data = str(sky_resp['predConcello']['listaPredDiaConcello'][0]['ceo']['manha'])
+      
+    code_sky = requests.get('https://irdgcdinfo.data.blog/codigos/')
+    soup2 = BeautifulSoup(code_sky.content.decode(), 'html.parser')
+    
+    dict2 = {}
+    for element2 in soup2.find_all('tr'):
+        columnas2 = element2.find_all('th')
+    
+        dict2[columnas2[0].get_text()] = columnas2[1].get_text()
+    
+    codCielo = columnas2[0].get_text()
+    sky_state = dict2[sky_data]
+    print('Estado del cielo por la mañana:', sky_state)
+    
+    coords_resp = requests.get('https://eu1.locationiq.com/v1/search.php?key=pk.e9566b1abfca4228171ce88e459b94aa&q='+townhall_name+'&format=xml')
+    coords_soup = BeautifulSoup(coords_resp.content.decode(), 'lxml')
+    
+    list_place = []
+    for element3 in coords_soup.find_all('place'):
+        list_place.append(element3)
+    importance = list_place[0]['importance']
+    lat = list_place[0]['lat']
+    lon = list_place[0]['lon']
+    
+    print('Latitud:', lat)
+    print('Longitud:', lon)
+    print('\n')
+    
+    points_list = [codCielo, [lon, lat]]
+    map_list.append(points_list)
+    time.sleep(1)
 
-sky_data = str(sky_resp['predConcello']['listaPredDiaConcello'][0]['ceo']['manha'])
-  
-code_sky = requests.get('https://irdgcdinfo.data.blog/codigos/')
-soup2 = BeautifulSoup(code_sky.content.decode(), 'html.parser')
-
-dict2 = {}
-for element2 in soup2.find_all('tr'):
-    columnas2 = element2.find_all('th')
-
-    dict2[columnas2[0].get_text()] = columnas2[1].get_text()
-
-  
-sky_state = dict2[sky_data]
-print('Estado del cielo por la mañana:', sky_state)
-
-coords_resp = requests.get('https://eu1.locationiq.com/v1/search.php?key=pk.e9566b1abfca4228171ce88e459b94aa&q='+townhall_name+'&format=xml')
-coords_soup = BeautifulSoup(coords_resp.content.decode(), 'lxml')
-
-list_place = []
-for element3 in coords_soup.find_all('place'):
-    list_place.append(element3)
-importance = list_place[0]['importance']
-lat = list_place[0]['lat']
-lon = list_place[0]['lon']
-
-print('Latitud:', lat)
-print('Longitud:', lon)
+gmg.plotMap(map_list)
     
